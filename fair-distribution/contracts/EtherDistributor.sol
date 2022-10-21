97a29c84c6010f75c56668212aafbb516f035e9b
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-10-21 14:07:01 +0300

    Formatted EtherDistributor

--- fair-distribution/contracts/EtherDistributor.sol
+++ fair-distribution/contracts/EtherDistributor.sol
@@ -2,32 +2,30 @@
 pragma solidity ^0.8.13;
 
 contract EtherDistributor {
+    struct User {
+        uint256 id;
+        address payable addr;
+        uint256 balance;
+        uint256 demandEpoch;
+        uint256 claimEpoch;
+    }
 
-	struct User {
-		uint id;
-		address addr;
-		uint balance;
-		uint demandEpoch;
-		uint claimEpoch;
-	}
+    address public owner;
+    uint256 public numberOfUsers;
+    User[] public permissionedUsers;
 
-	address public owner;
-	uint public numberOfUsers;
-	User[] public permissionedUsers;
+    constructor() {
+        owner = msg.sender;
+        numberOfUsers = 0;
+    }
 
-	constructor(){
-		owner = msg.sender;
-		numberOfUsers = 0;
-	}
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
+    }
 
-	modifier onlyOwner() {
-		require(msg.sender == owner, "Only owner can call this function.");
-		_;
-	}
-
-	function addPermissionedUser(address _addr) public onlyOwner {
-		permissionedUsers.push(User(numberOfUsers, _addr, 0, 0, 0));
-		numberOfUsers++;
-	}
-
+    function addPermissionedUser(address payable _addr) public onlyOwner {
+        permissionedUsers.push(User(numberOfUsers, _addr, 0, 0, 0));
+        numberOfUsers++;
+    }
 }
