09043edb3f914edb1203ee0e3c6b464ea4e9cec7
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-10-21 13:49:13 +0300

    Added permission functions to EtherDistributor

--- fair-distribution/contracts/EtherDistributor.sol
+++ fair-distribution/contracts/EtherDistributor.sol
@@ -3,4 +3,31 @@ pragma solidity ^0.8.13;
 
 contract EtherDistributor {
 
+	struct User {
+		uint id;
+		address addr;
+		uint balance;
+		uint demandEpoch;
+		uint claimEpoch;
+	}
+
+	address public owner;
+	uint public numberOfUsers;
+	User[] public permissionedUsers;
+
+	constructor(){
+		owner = msg.sender;
+		numberOfUsers = 0;
+	}
+
+	modifier onlyOwner() {
+		require(msg.sender == owner, "Only owner can call this function.");
+		_;
+	}
+
+	function addPermissionedUser(address _addr) public onlyOwner {
+		permissionedUsers.push(User(numberOfUsers, _addr, 0, 0, 0));
+		numberOfUsers++;
+	}
+
 }
