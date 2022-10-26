675998a6f7d60b1724f1477ccf1565b6646f2361
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-10-26 23:31:04 +0300

    Add cumulative capacity management

--- fair-distribution/contracts/EtherDistributor.sol
+++ fair-distribution/contracts/EtherDistributor.sol
@@ -17,15 +17,21 @@ contract EtherDistributor {
     mapping(address => User) public permissionedAddresses;
 
     uint16 public constant maxDemandVolume = 10;
-    uint256 public constant capacity = 50;
+    uint256 public constant epochCapacity = 5;
+    uint256 public cumulativeCapacity;
     uint16 public share; // calculated with _calculateShare()
 
     uint256[maxDemandVolume + 1] public numberOfDemands; // demand volume array
     uint256 public totalDemand; // total number of demands, D
 
+    uint256 public blockOffset; // block number of the contract creation
+    uint256 public constant epochDuration = 2000; // 2000 blocks
+    uint256 public epoch = 1; // epoch counter
+
     constructor() {
         owner = msg.sender;
         numberOfUsers = 0;
+        blockOffset = block.number;
     }
 
     modifier onlyOwner() {
@@ -61,14 +67,38 @@ contract EtherDistributor {
     }
 
     function _updateState() internal {
-        share = _calculateShare();
+        if (epoch < (block.number - blockOffset) / epochDuration) {
+            // if the current epoch is over
+            epoch = (block.number - blockOffset) / epochDuration;
+            uint256 distribution;
+            (share, distribution) = _calculateShare();
+            cumulativeCapacity -= distribution; // subtract the distributed amount
+            cumulativeCapacity += epochCapacity; // add the capacity of the new epoch
+        }
+        // TODO: refund the remaining gas to the caller
     }
 
-    // only called by _updateState, hence, assumes that the state is updated
-    function _calculateShare() internal view returns (uint16) {
+    function _calculateShare()
+        internal
+        view
+        returns (uint16 _share, uint256 _amount)
+    {
+        /*
+         * This function calculates the maximum share that can be distributed
+         * in the current epoch to the users. In addition to that,it also
+         * calculates the total distribution amount for the calculated maximum
+         * share.
+         *
+         * These two values mentioned above are returned in a tuple as (share, amount).
+         *
+         * Note: only called by _updateState(), hence, assumes that the state is updated
+         */
+
         uint256 cumulativeNODSum = 0;
         uint256 cumulativeTDVSum = 0;
-        uint256 necessaryCapacity = 0;
+
+        uint256 necessaryCapacity = 0; // necessary capacity to meet demands at ith volume
+        uint256 sufficientCapacity = 0; // the latest necessaryCapacity that can be distributed
 
         for (uint16 i = 1; i <= maxDemandVolume; i++) {
             uint256 currentNOD = numberOfDemands[i];
@@ -76,23 +106,29 @@ contract EtherDistributor {
                 // skip calculations if there is no demand
                 continue;
             }
-            
+
             // use the previous values of cumulativeNODSum and cumulativeTDVSum
             necessaryCapacity =
                 cumulativeTDVSum +
                 i *
                 (totalDemand - cumulativeNODSum);
-            
+
             // then calculate the new values
             cumulativeNODSum += currentNOD;
             cumulativeTDVSum += currentNOD * i;
 
-            if (necessaryCapacity > capacity) {
-                return i - 1;
+            if (necessaryCapacity > cumulativeCapacity) {
+                // necessaryCapacity for this volume is larger than the cumulativeCapacity
+                // so, sufficientCapacity stores the maximum amount that can be distributed
+                return (i - 1, sufficientCapacity);
+            } else {
+                // current capacity is sufficient
+                sufficientCapacity = necessaryCapacity;
             }
         }
 
-        return maxDemandVolume;
+        // cumulative capacity was enough for all demands
+        return (maxDemandVolume, necessaryCapacity);
     }
 
     function min(uint256 a, uint256 b) internal pure returns (uint256) {
