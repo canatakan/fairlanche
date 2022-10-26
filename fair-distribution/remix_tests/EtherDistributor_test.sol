d32068690f409ccb2c0cd3e42e78a4a33ef6f6f4
Remix Workspace <>
2022-10-27 00:52:51 +0300

    Add basic Remix tests

---
+++ fair-distribution/remix_tests/EtherDistributor_test.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0
+        
+pragma solidity >=0.4.22 <0.9.0;
+
+// This import is automatically injected by Remix
+import "remix_tests.sol"; 
+
+// This import is required to use custom transaction context
+// Although it may fail compilation in 'Solidity Compiler' plugin
+// But it will work fine in 'Solidity Unit Testing' plugin
+import "remix_accounts.sol";
+import "../contracts/EtherDistributor.sol";
+
+// File name has to end with '_test.sol', this file can contain more than one testSuite contracts
+contract testSuite {
+
+    EtherDistributor etherDistributor;
+    
+    // 'beforeAll' runs before all other tests
+    function beforeAll() public {
+        etherDistributor = new EtherDistributor();
+        etherDistributor.addPermissionedUser(payable(address(this)));
+    }
+
+    function checkOwner() public {
+        Assert.equal(etherDistributor.owner(), payable(address(this)), "Invalid owner");
+    }
+
+    function checkDemands() public {
+        uint8[13] memory demandArray = [1, 1, 1, 2, 2, 3, 5, 5, 5, 7, 7, 7, 7];
+        for(uint256 i = 0; i < demandArray.length; i++) {
+            etherDistributor.demand(demandArray[i]);
+        }
+        Assert.equal(etherDistributor.totalDemand(), 13, "Invalid demand count");
+    }
+
+    function checkCalculateShare() public {
+
+        (uint16 share, uint256 distribution) =  etherDistributor._calculateShare();
+        
+        Assert.equal(share, 6, "Invalid share");
+        Assert.equal(distribution, 49, "Invalid distribution amount");
+    }
+
+}
+    
