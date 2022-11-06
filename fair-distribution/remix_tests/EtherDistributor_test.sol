4bba4ba7a12e1339dfc04ab7d4087af70b02097d
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-11-06 23:48:15 +0300

    Fix test instance

--- fair-distribution/remix_tests/EtherDistributor_test.sol
+++ fair-distribution/remix_tests/EtherDistributor_test.sol
@@ -9,6 +9,7 @@ import "remix_tests.sol";
 // Although it may fail compilation in 'Solidity Compiler' plugin
 // But it will work fine in 'Solidity Unit Testing' plugin
 import "remix_accounts.sol";
+
 import "./TestEtherDistributor.sol";
 
 // File name has to end with '_test.sol', this file can contain more than one testSuite contracts
@@ -17,7 +18,7 @@ contract testSuite {
 
     // 'beforeAll' runs before all other tests
     function beforeAll() public {
-        etherDistributor = new EtherDistributor();
+        etherDistributor = new TestEtherDistributor();
         etherDistributor.addPermissionedUser(payable(address(this)));
     }
 
