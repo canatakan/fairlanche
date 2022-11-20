895f00ce95443e696188aa00032e2446c072666e
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-11-20 14:56:05 +0300

    Add User Registration tests

--- hardhat-project/test/EtherDistributor.js
+++ hardhat-project/test/EtherDistributor.js
@@ -28,4 +28,41 @@ describe("EtherDistributor contract", function () {
     });
   });
 
+  describe("User Registration", function () {
+    it("Should add 5 permissioned users", async function () {
+      const accounts = await ethers.getSigners();
+      
+      // register accounts 1-5, 0 is the owner
+      for (i = 1; i < 6; i++) {
+        await this.etherDistributor.addPermissionedUser(accounts[i].address);
+      }
+
+      // check that 5 users are registered
+      expect(await this.etherDistributor.numberOfUsers()).to.equal(5);
+
+      // check that 5 users are registered with the correct data
+      for (i = 1; i < 6; i++) {
+        const user =  await this.etherDistributor.permissionedAddresses((accounts[i].address)) 
+        expect(user.id).to.equal(i);
+        expect(user.addr).to.equal(accounts[i].address);
+      }
+
+      // check that some random accounts are not registered
+      for (i = 6; i < 10; i++) {
+        const user =  await this.etherDistributor.permissionedAddresses((accounts[i].address)) 
+        expect(user.id).to.equal(0);
+        expect(user.addr).to.equal("0x0000000000000000000000000000000000000000");
+      }
+    });
+
+    it("Should fail while adding a user that is already registered", async function () {
+      const accounts = await ethers.getSigners();
+      await expect(this.etherDistributor.addPermissionedUser(accounts[1].address)).to.be.revertedWith("User already exists.");
+    });
+
+    it("Should fail when someone other than the owner tries to register a user", async function () {
+      const accounts = await ethers.getSigners();
+      await expect(this.etherDistributor.connect(accounts[1]).addPermissionedUser(accounts[9].address)).to.be.revertedWith("Only owner can call this function.");
+    });
+  });
 });
