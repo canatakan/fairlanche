81fb8adbfed8ecfc2ef4eb9d5b77ba8534958c2c
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-11-20 14:14:37 +0300

    Add ordering in `accounts` task

--- hardhat-project/hardhat.config.js
+++ hardhat-project/hardhat.config.js
@@ -7,8 +7,9 @@ module.exports = {
 
 task("accounts", "Prints the list of accounts with balances", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
-  for (const account of accounts) {
+  for (i = 0; i < accounts.length; i++) {
+    const account = accounts[i];
     const balance = ethers.utils.formatEther(await account.getBalance());
-    console.log(account.address, "(" + balance + " ETH)");
+    console.log("[" + i + "]", account.address, "(" + balance + " ETH)");
   }
 });
