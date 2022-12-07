cc5f944877f1d92ed12c5d0303106b7deb1ac334
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-12-08 02:40:54 +0300

    Add basic deployment structure

--- hardhat-project/hardhat.config.js
+++ hardhat-project/hardhat.config.js
@@ -1,11 +1,43 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+
+const dotenv = require("dotenv")
+dotenv.config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      accounts: {
+        count: 20,
+      },
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: [process.env.AVALANCHE_TEST_PRIVATE_KEY],
+      chainId: 43113,
+    },
+    avalanche: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      accounts: [process.env.AVALANCHE_PRIVATE_KEY],
+      chainId: 43114,
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      avalanche: process.env.ETHERSCAN_API_KEY,
+      avalancheFujiTestnet: process.env.ETHERSCAN_API_KEY,
+    }
+  },
 };
 
-task("accounts", "Prints the list of accounts with balances", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts with balances", async (_, hre) => {
   const accounts = await hre.ethers.getSigners();
   for (i = 0; i < accounts.length; i++) {
     const account = accounts[i];
--- hardhat-project/scripts/deploy.js
+++ hardhat-project/scripts/deploy.js
@@ -2,7 +2,12 @@ const hre = require("hardhat"); // Hardhat Runtime Environment
 
 async function main() {
   const EtherDistributor = await hre.ethers.getContractFactory("EtherDistributor");
-  const etherDistributor = await EtherDistributor.deploy();
+  const etherDistributor = await EtherDistributor.deploy(
+    3,
+    100,
+    enableWithdrawal,
+    { value: ethers.utils.parseEther("10.0") }
+  );
   await etherDistributor.deployed();
   console.log("EtherDistributor deployed to:", etherDistributor.address);
 }
