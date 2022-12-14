342ebc06b8a110d0538197345efe884d940db2ff
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-12-14 20:04:42 +0300

    Add wallet disconnection

--- front-end/src/components/WalletConnector.js
+++ front-end/src/components/WalletConnector.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { useEtherBalance, useEthers } from '@usedapp/core'
+import { useEthers } from '@usedapp/core'
 
 
 function WalletConnector() {
 
-  const { account, activateBrowserWallet } = useEthers()
+  const { account, deactivate, activateBrowserWallet } = useEthers()
 
   return (
     <div className="flex items-center">
@@ -18,6 +18,14 @@ function WalletConnector() {
             <div className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                 {account.slice(0, 6) + '...' + account.slice(-4)}
             </div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                deactivate()
+              }}
+            >
+              Disconnect
+            </button>
           </div>
         </div>
       ) : (
