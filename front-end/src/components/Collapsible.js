982550cd3a825a35e3b68eaeab04ab0f60eadd9c
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-12-14 20:04:56 +0300

    Implement basic Transaction Page

---
+++ front-end/src/components/Collapsible.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronUp,
+  faChevronDown
+} from "@fortawesome/fontawesome-free-solid";
+
+const Collapsible= ({ open, children, title }) => {
+  const [isOpen, setIsOpen] = useState(open);
+
+  const handleFilterOpening = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div className="card">
+        <div>
+          <div className="p-3 border-bottom flex justify-between">
+            <h6 className="font-weight-bold">{title}</h6>
+            <button type="button" className="btn" onClick={handleFilterOpening}>
+              {!isOpen ? (
+                <FontAwesomeIcon icon={faChevronDown} />
+              ) : (
+                <FontAwesomeIcon icon={faChevronUp} />
+              )}
+            </button>
+          </div>
+        </div>
+        <div className="border-bottom">
+          <div>{isOpen && <div className="p-3">{children}</div>}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Collapsible;
--- front-end/src/components/pages/Transaction.js
+++ front-end/src/components/pages/Transaction.js
@@ -1,11 +1,80 @@
-import React from "react";
+import React from 'react';
 
-function TransactionPage() {
-  return (
-    <div>
-      <h1>Transaction</h1>
-    </div>
-  );
+import Collapsible from '../Collapsible';
+
+class TransactionPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      contractAddresses: [],
+    };
+  }
+
+  componentDidMount() {
+    const contractAddresses = JSON.parse(localStorage.getItem('contractAddresses')) || [];
+    this.setState({ contractAddresses });
+  }
+
+  saveContractAddress = (event) => {
+    event.preventDefault();
+    const contractAddress = event.target.elements.contractAddress.value;
+    const contractAddresses = [...this.state.contractAddresses, contractAddress];
+    this.setState({ contractAddresses });
+    localStorage.setItem('contractAddresses', JSON.stringify(contractAddresses));
+  }
+
+  removeContractAddress = (contractAddress) => {
+    const contractAddresses = this.state.contractAddresses.filter((address) => address !== contractAddress);
+    this.setState({ contractAddresses });
+    localStorage.setItem('contractAddresses', JSON.stringify(contractAddresses));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="flex justify-center">
+          <h1 className="text-3xl font-bold">Transaction Page</h1>
+        </div>
+        <div className="form">
+          <form onSubmit={this.saveContractAddress}>
+            <input type="text" name="contractAddress" placeholder='Contract Address'/>
+            <button type="submit">Save</button>
+          </form>
+        </div>
+        <ul>
+          {this.state.contractAddresses.map((contractAddress) => (
+             <div className='border-2 border-black'>
+              <Collapsible
+             open
+             title=<div className='text-xl font-bold'>{contractAddress}</div>
+           >
+              <div className='flex flex-col items-center justify-center'>
+                <button type="button">
+                  <input type="number" name="volume" placeholder='Volume'/>
+                  demand
+                </button>
+                <br></br>
+                <button type="button">
+                  <input type="number" name="epochNumber" placeholder='Epoch Number'/>
+                  claim
+                </button>
+                <br></br>
+                <button type="button">
+                  claimAll
+                </button>
+                <br></br>
+                <button type="button" onClick={() => this.removeContractAddress(contractAddress)}>
+                  Remove Contract
+                </button>
+              </div>
+           </Collapsible>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default TransactionPage;
