ebac4f24c0e4aa79dc2026323f81c34684e59953
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-12-15 17:47:04 +0300

    Validate contract addresses

--- front-end/src/components/pages/ContractPage.js
+++ front-end/src/components/pages/ContractPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
 
+import { ethers } from "ethers";
+
 import Collapsible from '../Collapsible';
 
 function withParams(Component) {
@@ -23,10 +25,30 @@ class ContractPage extends React.Component {
 
   saveContractAddress = (event) => {
     event.preventDefault();
+    if (!this.verifyContractAddress(event)) {
+      return;
+    }
     const contractAddress = event.target.elements.contractAddress.value;
     const contractAddresses = [...this.state.contractAddresses, contractAddress];
     this.setState({ contractAddresses });
     localStorage.setItem('contractAddresses', JSON.stringify(contractAddresses));
+  }
+
+  verifyContractAddress = (event) => {
+    event.preventDefault();
+    const contractAddress = event.target.elements.contractAddress.value;
+
+    if (this.state.contractAddresses.includes(contractAddress)) {
+      alert('Duplicate contract address');
+      return false;
+    }
+
+    if (!ethers.utils.isAddress(contractAddress)) {
+      alert('Invalid contract address');
+      return false;
+    }
+
+    return true;
   }
 
   removeContractAddress = (contractAddress) => {
@@ -43,41 +65,47 @@ class ContractPage extends React.Component {
         </div>
         <div className="form">
           <form onSubmit={this.saveContractAddress}>
-            <input type="text" name="contractAddress" placeholder='Contract Address'/>
+            <input type="text" name="contractAddress" placeholder='Contract Address' />
             <button className='mt-1 mb-2'>Save</button>
           </form>
         </div>
         <ul>
           {this.state.contractAddresses.map((contractAddress) => (
-             <div className='border-2 border-black'>
+            <div className='border-2 border-black'>
               <Collapsible
-             open
-             title=<div className='text-xl font-bold text-center mb-2 hover:text-blue-600 focus:text-blue-600'>{contractAddress}</div>
-           >
-              <div className='flex flex-col items-center justify-center'>
-                
-                <div className='flex flex-row items-center justify-center p-2'>
-                <input type="number" name="volume" placeholder='Volume'/>
-                <button>
-                  demand
-                </button>
+                open
+                title=
+                <div className='flex flex-row items-center justify-center'>
+                  <a href={`https://testnet.snowtrace.io/address/${contractAddress}`} target="_blank" rel="noopener noreferrer">
+                    {<div className='text-xl font-bold text-center mb-2 hover:text-blue-600 focus:text-blue-600'>{contractAddress}</div>
+                    }
+                  </a>
                 </div>
-                <div className='flex flex-row items-center justify-center p-2'>
-                <input type="number" name="epochNumber" placeholder='Epoch Number'/>
-                <button>
-                  claim
-                </button>
+              >
+                <div className='flex flex-col items-center justify-center'>
+
+                  <div className='flex flex-row items-center justify-center p-2'>
+                    <input type="number" name="volume" placeholder='Volume' />
+                    <button>
+                      demand
+                    </button>
+                  </div>
+                  <div className='flex flex-row items-center justify-center p-2'>
+                    <input type="number" name="epochNumber" placeholder='Epoch Number' />
+                    <button>
+                      claim
+                    </button>
+                  </div>
+                  <div className='flex flex-row items-center justify-center p-2'>
+                    <button>
+                      claimAll
+                    </button>
+                  </div>
+                  <button onClick={() => this.removeContractAddress(contractAddress)}>
+                    Remove Contract
+                  </button>
                 </div>
-                <div className='flex flex-row items-center justify-center p-2'>
-                <button>
-                  claimAll
-                </button>
-                </div>
-                <button onClick={() => this.removeContractAddress(contractAddress)}>
-                  Remove Contract
-                </button>
-              </div>
-           </Collapsible>
+              </Collapsible>
             </div>
           ))}
         </ul>
