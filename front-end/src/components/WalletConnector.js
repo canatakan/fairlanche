3096c79a3eedeed4dffa5e1440271e5d6b4ef909
cagin24 <cagin24@gmail.com>
2022-12-18 21:49:07 +0300

    fix Wallet Connector

--- front-end/src/components/WalletConnector.js
+++ front-end/src/components/WalletConnector.js
@@ -2,58 +2,44 @@ import React from "react";
 
 import { AvalancheTestnet, useEthers } from '@usedapp/core'
 
-
-function WalletConnector() {
+export default function WalletConnector() {
 
   const { account, deactivate, activateBrowserWallet, chainId, switchNetwork } = useEthers()
 
-  if (chainId !== AvalancheTestnet.chainId) {
-
+  if (!account) {
     return (
-      <div className="flex items-center">
-        <button
-          onClick={() => {
-            switchNetwork(AvalancheTestnet.chainId)
-          }}
-        >
-          Switch to Fuji Testnet
-        </button>
-      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={activateBrowserWallet}
+      >
+        Connect Wallet
+      </button>
     )
   }
-  else {
+
+  if (chainId !== AvalancheTestnet.chainId) {
+    return (
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => switchNetwork(AvalancheTestnet.chainId)}
+      >
+        Switch to Fuji Testnet
+      </button>
+    )
+  }
+
   return (
-    <div className="flex items-center">
-      {account ? (
-        <div className="flex items-center">
-          <div className="flex items-center">
-            <img className="block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
-          </div>
-          <div className="flex space-x-4">
-            <div className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                {account.slice(0, 6) + '...' + account.slice(-4)}
-            </div>
-            <button
-              onClick={() => {
-                deactivate()
-              }}
-            >
-              Disconnect
-            </button>
-          </div>
-        </div>
-      ) : (
-        <button
-          onClick={() => {
-            activateBrowserWallet()
-          }}
-        >
-          Connect Wallet
-        </button>
-      )}
+    <div className="flex space-x-4">
+      <div className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+        {account.slice(0, 6) + '...' + account.slice(-4)}
+      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={deactivate}
+      >
+        Disconnect Wallet
+      </button>
     </div>
-  );
-}
+  )
 }
 
-export default WalletConnector;
