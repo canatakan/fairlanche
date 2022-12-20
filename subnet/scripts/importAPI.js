00f276e2b0091dabae73494b26b385c74dfd249d
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-12-20 04:59:57 +0300

    Add individual node

--- subnet/scripts/importAPI.js
+++ subnet/scripts/importAPI.js
@@ -8,34 +8,38 @@ const bintools = BinTools.getInstance()
 
 // Avalanche instance with 3rd party API endpoints
 const skip = (num) => new Array(num);
-const avalanche = new Avalanche(...skip(3), 5, ...skip(2), "fuji")
+// const avalanche = new Avalanche(...skip(3), 5, ...skip(2), "fuji")
 
-const platform = avalanche.PChain()
-platform.setBaseURL(pBase)
-const xchain = avalanche.XChain()
-xchain.setBaseURL(xBase)
-const cchain = avalanche.CChain()
-cchain.setBaseURL(cBase)
+// const platform = avalanche.PChain()
+// platform.setBaseURL(pBase)
+// const xchain = avalanche.XChain()
+// xchain.setBaseURL(xBase)
+// const cchain = avalanche.CChain()
+// cchain.setBaseURL(cBase)
+
+//info.setBaseURL(cBase)
 
 // Avalanche instance with independent node
-// const avalanche = new Avalanche(
-//   "0.0.0.0", // ip
-//   14760, // port
-//   "http", // protocol
-//   5, // fuji: 43113, mainnet: 43114, local: 1337
-// )
-//
-// const platform = avalanche.PChain()
-// const xchain = avalanche.XChain()
-// const cchain = avalanche.CChain()
+const avalanche = new Avalanche(
+  "18.188.152.131", // ip
+  9650, // port
+  "http", // protocol
+  5, // avax: 1, fuji: 5, custom: 1337, local: 12345
+  ...skip(2), // xchainID, cchainID
+  "fuji" // hrt
+)
+
+const platform = avalanche.PChain()
+const xchain = avalanche.XChain()
+const cchain = avalanche.CChain()
 
 const info = avalanche.Info()
-info.setBaseURL(cBase)
 
 // Keychain for signing transactions
 const pKeyChain = platform.keyChain()
 pKeyChain.importKey(privKey)
 const pAddressStrings = pKeyChain.getAddressStrings()
+console.log("Platform address: ", pAddressStrings[0])
 
 // UTXOs for spending unspent outputs
 const utxoSet = async () => {
