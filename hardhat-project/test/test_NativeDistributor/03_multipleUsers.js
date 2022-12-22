2ba033600dbed03d23eaa9a71f20a51c12093d9b
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-12-22 21:32:11 +0300

    Minor update in multiple user tests

--- hardhat-project/test/test_NativeDistributor/03_multipleUsers.js
+++ hardhat-project/test/test_NativeDistributor/03_multipleUsers.js
@@ -1,3 +1,16 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { mine, loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+
+const {
+  deployDistributor,
+  demandBulk,
+  permissionedDeploymentFixture
+} = require("../test_utils/deployment");
+
+const {
+  DEFAULT_ETHER_MULTIPLIER,
+} = require("../test_utils/config");
 
 describe("Multiple users", function () {
 
@@ -8,35 +21,35 @@ describe("Multiple users", function () {
       describe("Share updates", function () {
 
         it("Should be able to update the share 1 epoch later", async function () {
-          const { nativeDistributor } = await loadFixture(permissionedDeploymentFixture);
+          let { nativeDistributor } = await loadFixture(permissionedDeploymentFixture);
           await demandBulk(nativeDistributor, await ethers.getSigners(), [1, 1, 1, 2, 2, 3, 5, 5, 5, 7, 7, 7, 7]);
           await mine(DEFAULT_EPOCH_DURATION);
           await nativeDistributor._updateState();
 
           // check that the share is correctly stored in the array
-          const share = await nativeDistributor.shares(1);
+          let share = await nativeDistributor.shares(1);
           expect(share).to.equal(6);
         });
 
         it("Should be able to update the share multiple epochs later", async function () {
-          const { nativeDistributor } = await loadFixture(permissionedDeploymentFixture);
+          let { nativeDistributor } = await loadFixture(permissionedDeploymentFixture);
           await demandBulk(nativeDistributor, await ethers.getSigners(), [1, 1, 1, 2, 2, 3, 5, 5, 5, 7, 7, 7, 7]);
           await mine(DEFAULT_EPOCH_DURATION * 5);
           await nativeDistributor._updateState();
 
           // check that the share is correctly stored in the array
-          const share = await nativeDistributor.shares(1);
+          let share = await nativeDistributor.shares(1);
           expect(share).to.equal(6);
         });
 
         it("Should not update the share before the epoch is over", async function () {
-          const { nativeDistributor } = await loadFixture(permissionedDeploymentFixture);
+          let { nativeDistributor } = await loadFixture(permissionedDeploymentFixture);
           await demandBulk(nativeDistributor, await ethers.getSigners(), [1, 1, 1, 2, 2, 3, 5, 5, 5, 7, 7, 7, 7]);
           await mine(22);
           await nativeDistributor._updateState();
 
           // check that the share is correctly stored in the array
-          const share = await nativeDistributor.shares(1);
+          let share = await nativeDistributor.shares(1);
           expect(share).to.equal(0);
         });
       });
@@ -46,9 +59,9 @@ describe("Multiple users", function () {
         const customEpochCapacities = [50, 48, 90, 5, 1];
 
         it("50: [1, 1, 1, 2, 2, 3, 5, 5, 5, 7, 7, 7, 7]", async function () {
-          const customEpochCapacity = customEpochCapacities[0];
-          const { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, DEFAULT_DEPLOYMENT_VALUE);
-          const accounts = await ethers.getSigners();
+          let customEpochCapacity = customEpochCapacities[0];
+          let { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, DEFAULT_DEPLOYMENT_VALUE);
+          let accounts = await ethers.getSigners();
           for (i = 1; i <= 13; i++) {
             nativeDistributor.addPermissionedUser(accounts[i].address);
           }
@@ -59,7 +72,7 @@ describe("Multiple users", function () {
           await nativeDistributor._updateState();
 
           // check that the share is correctly stored in the array
-          const share = await nativeDistributor.shares(1);
+          let share = await nativeDistributor.shares(1);
           expect(share).to.equal(6);
 
           // check whether the distribution is 49
@@ -67,9 +80,9 @@ describe("Multiple users", function () {
         });
 
         it("48: [1, 1, 1, 2, 2, 3, 5, 5, 5, 7, 7, 7, 7]", async function () {
-          const customEpochCapacity = customEpochCapacities[1];
-          const { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, DEFAULT_DEPLOYMENT_VALUE);
-          const accounts = await ethers.getSigners();
+          let customEpochCapacity = customEpochCapacities[1];
+          let { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, DEFAULT_DEPLOYMENT_VALUE);
+          let accounts = await ethers.getSigners();
           for (i = 1; i <= 13; i++) {
             nativeDistributor.addPermissionedUser(accounts[i].address);
           }
@@ -79,7 +92,7 @@ describe("Multiple users", function () {
           await nativeDistributor._updateState();
 
           // check that the share is correctly stored in the array
-          const share = await nativeDistributor.shares(1);
+          let share = await nativeDistributor.shares(1);
           expect(share).to.equal(5);
 
           // check whether the distribution is 45
@@ -87,9 +100,9 @@ describe("Multiple users", function () {
         });
 
         it("90: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]", async function () {
-          const customEpochCapacity = customEpochCapacities[2];
-          const { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, DEFAULT_DEPLOYMENT_VALUE);
-          const accounts = await ethers.getSigners();
+          let customEpochCapacity = customEpochCapacities[2];
+          let { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, DEFAULT_DEPLOYMENT_VALUE);
+          let accounts = await ethers.getSigners();
           for (i = 1; i <= 10; i++) {
             nativeDistributor.addPermissionedUser(accounts[i].address);
           }
@@ -100,7 +113,7 @@ describe("Multiple users", function () {
           await nativeDistributor._updateState();
 
           // check that the share is correctly stored in the array
-          const share = await nativeDistributor.shares(1);
+          let share = await nativeDistributor.shares(1);
           expect(share).to.equal(9);
 
           // check whether the distribution is 90, no excessives
@@ -108,9 +121,9 @@ describe("Multiple users", function () {
         });
 
         it("5: [5, 2, 2, 5, 1]", async function () {
-          const customEpochCapacity = customEpochCapacities[3];
-          const { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, DEFAULT_DEPLOYMENT_VALUE);
-          const accounts = await ethers.getSigners();
+          let customEpochCapacity = customEpochCapacities[3];
+          let { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, DEFAULT_DEPLOYMENT_VALUE);
+          let accounts = await ethers.getSigners();
           for (i = 1; i <= 5; i++) {
             nativeDistributor.addPermissionedUser(accounts[i].address);
           }
@@ -121,7 +134,7 @@ describe("Multiple users", function () {
           await nativeDistributor._updateState();
 
           // check that the share is correctly stored in the array
-          const share = await nativeDistributor.shares(1);
+          let share = await nativeDistributor.shares(1);
           expect(share).to.equal(1);
 
           // check whether the distribution is 5, no excessives
@@ -129,15 +142,15 @@ describe("Multiple users", function () {
         });
 
         it("1: [1, 1, 1, ... , 1]", async function () {
-          const customEpochCapacity = customEpochCapacities[3];
-          const { nativeDistributor } = await deployDistributor(customEpochCapacity);
-          const accounts = await ethers.getSigners();
+          let customEpochCapacity = customEpochCapacities[3];
+          let { nativeDistributor } = await deployDistributor(customEpochCapacity);
+          let accounts = await ethers.getSigners();
           for (i = 1; i <= 19; i++) {
             nativeDistributor.addPermissionedUser(accounts[i].address);
           }
           await mine(1);
 
-          const demandArray = [];
+          let demandArray = [];
           for (i = 0; i < 19; i++) {
             demandArray.push(1);
           }
@@ -147,7 +160,7 @@ describe("Multiple users", function () {
           await nativeDistributor._updateState();
 
           // check that the share is correctly stored in the array
-          const share = await nativeDistributor.shares(1);
+          let share = await nativeDistributor.shares(1);
           expect(share).to.equal(0);
 
           // check whether the distribution is 0, all balance to the next epoch
@@ -158,9 +171,9 @@ describe("Multiple users", function () {
 
     describe("Multiple epochs (Cumulative)", function () {
       it("Should apply Calculation 1 for 2 epochs", async function () {
-        const customEpochCapacity = 50;
-        const { nativeDistributor } = await deployDistributor(customEpochCapacity);
-        const accounts = await ethers.getSigners();
+        let customEpochCapacity = 50;
+        let { nativeDistributor } = await deployDistributor(customEpochCapacity);
+        let accounts = await ethers.getSigners();
         for (i = 1; i <= 13; i++) {
           nativeDistributor.addPermissionedUser(accounts[i].address);
         }
@@ -188,9 +201,9 @@ describe("Multiple users", function () {
       });
 
       it("Should apply Calculation 2 for 5 epochs", async function () {
-        const customEpochCapacity = 48;
-        const { nativeDistributor } = await deployDistributor(customEpochCapacity);
-        const accounts = await ethers.getSigners();
+        let customEpochCapacity = 48;
+        let { nativeDistributor } = await deployDistributor(customEpochCapacity);
+        let accounts = await ethers.getSigners();
         for (i = 1; i <= 13; i++) {
           nativeDistributor.addPermissionedUser(accounts[i].address);
         }
@@ -203,8 +216,8 @@ describe("Multiple users", function () {
         * Epoch 4: cumulativeCapacity = 49, share = 6, distribution = 49, excess = 0
         * Epoch 5: cumulativeCapacity = 48, share = 5, distribution = 45, excess = 3
         */
-        const expectedShares = [5, 6, 6, 6, 5];
-        const excessAmounts = [3, 2, 1, 0, 3];
+        let expectedShares = [5, 6, 6, 6, 5];
+        let excessAmounts = [3, 2, 1, 0, 3];
         for (j = 0; j < 5; j++) {
           await demandBulk(nativeDistributor, await ethers.getSigners(), [1, 1, 1, 2, 2, 3, 5, 5, 5, 7, 7, 7, 7]);
           await mine(DEFAULT_EPOCH_DURATION);
@@ -218,9 +231,9 @@ describe("Multiple users", function () {
       });
 
       it("Should apply Calculation 3 for 50 epochs", async function () {
-        const customEpochCapacity = 90;
-        const { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, 4500, false);
-        const accounts = await ethers.getSigners();
+        let customEpochCapacity = 90;
+        let { nativeDistributor } = await deployDistributor(customEpochCapacity, DEFAULT_EPOCH_DURATION, 4500, false);
+        let accounts = await ethers.getSigners();
         for (i = 1; i <= 10; i++) {
           nativeDistributor.addPermissionedUser(accounts[i].address);
         }
@@ -246,7 +259,7 @@ describe("Multiple users", function () {
 
     describe("Claim single share", function () {
 
-      const demandArray = [1, 1, 1, 2, 2, 3, 5, 5, 5, 7, 7, 7, 7];
+      let demandArray = [1, 1, 1, 2, 2, 3, 5, 5, 5, 7, 7, 7, 7];
       let nativeDistributor;
       this.beforeAll(async function () {
         ({ nativeDistributor } = await loadFixture(permissionedDeploymentFixture));
@@ -256,40 +269,40 @@ describe("Multiple users", function () {
       it("Should allow everyone to claim their calculated shares", async function () {
         await mine(DEFAULT_EPOCH_DURATION);
         await nativeDistributor._updateState();
-        const accounts = await ethers.getSigners();
-        const claimEpoch = 1;
-        const expectedUserShares = [1, 1, 1, 2, 2, 3, 5, 5, 5, 6, 6, 6, 6]; // for capacity 50
+        let accounts = await ethers.getSigners();
+        let claimEpoch = 1;
+        let expectedUserShares = [1, 1, 1, 2, 2, 3, 5, 5, 5, 6, 6, 6, 6]; // for capacity 50
         for (i = 1; i <= 13; i++) {
-          const share = await nativeDistributor.shares(claimEpoch);
+          let share = await nativeDistributor.shares(claimEpoch);
 
-          const userInfo = await nativeDistributor.getUser(accounts[i].address);
-          const userDemand = userInfo[3][claimEpoch];
+          let userInfo = await nativeDistributor.getUser(accounts[i].address);
+          let userDemand = userInfo[3][claimEpoch];
           expect(userDemand).to.equal(demandArray[i - 1]); // demand is correct
 
-          const userShare = Math.min(share, userDemand);
+          let userShare = Math.min(share, userDemand);
           expect(userShare).to.equal(expectedUserShares[i - 1]); // calculated share is correct
 
-          const initialBalance = await ethers.provider.getBalance(accounts[i].address);
+          let initialBalance = await ethers.provider.getBalance(accounts[i].address);
 
-          const tx = await nativeDistributor.connect(accounts[i]).claim(claimEpoch);
-          const userBalance = await ethers.provider.getBalance(accounts[i].address);
-          const txReceipt = await ethers.provider.getTransactionReceipt(tx.hash);
-          const gasUsed = txReceipt.gasUsed;
-          const gasPrice = tx.gasPrice;
-          const gasCost = gasUsed.mul(gasPrice);
+          let tx = await nativeDistributor.connect(accounts[i]).claim(claimEpoch);
+          let userBalance = await ethers.provider.getBalance(accounts[i].address);
+          let txReceipt = await ethers.provider.getTransactionReceipt(tx.hash);
+          let gasUsed = txReceipt.gasUsed;
+          let gasPrice = tx.gasPrice;
+          let gasCost = gasUsed.mul(gasPrice);
 
-          const userInfoAfterClaim = await nativeDistributor.getUser(accounts[i].address);
+          let userInfoAfterClaim = await nativeDistributor.getUser(accounts[i].address);
           expect(userInfoAfterClaim[3][claimEpoch]).to.equal(0); // demand is reset to 0
 
-          const claimAmountWei = ethers.utils.parseEther(userShare.toString());
+          let claimAmountWei = ethers.utils.parseEther(userShare.toString());
           expect(userBalance).to.equal(initialBalance.add(claimAmountWei).sub(gasCost)); // transfer is correct
         }
       });
 
       it("Should not allow anyone without a share to claim", async function () {
-        const accounts = await ethers.getSigners();
-        const userInfo = await nativeDistributor.getUser(accounts[19].address);
-        const userDemand = userInfo[3][1];
+        let accounts = await ethers.getSigners();
+        let userInfo = await nativeDistributor.getUser(accounts[19].address);
+        let userDemand = userInfo[3][1];
         expect(userDemand).to.equal(0); // demand is 0
         await expect(nativeDistributor.connect(accounts[19]).claim(1)).to.be.revertedWith("You do not have a demand for this epoch.");
       });
@@ -298,18 +311,18 @@ describe("Multiple users", function () {
 
     describe("Claim all shares", function () {
 
-      const demandArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      let demandArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
       let nativeDistributor;
       this.beforeAll(async function () {
         ({ nativeDistributor } = await deployDistributor(55, DEFAULT_EPOCH_DURATION, 2000, false));
-        const accounts = await ethers.getSigners();
+        let accounts = await ethers.getSigners();
         for (i = 1; i <= 10; i++) {
           nativeDistributor.addPermissionedUser(accounts[i].address);
         }
       });
 
       it("Should allow everyone to call claimAll() after 10 epochs of demanding", async function () {
-        const accounts = await ethers.getSigners();
+        let accounts = await ethers.getSigners();
 
         /*
          * Demands for 10 epochs:
@@ -334,21 +347,21 @@ describe("Multiple users", function () {
 
         // check demands before claiming
         for (i = 1; i <= 10; i++) {
-          const userInfo = await nativeDistributor.getUser(accounts[i].address);
+          let userInfo = await nativeDistributor.getUser(accounts[i].address);
           for (j = 1; j <= 10; j++) {
             expect(userInfo[3][j]).to.equal(demandArray[(i + j - 2) % 10]);
           }
         }
 
         for (i = 1; i <= 10; i++) {
-          const initialBalance = await ethers.provider.getBalance(accounts[i].address);
-          const tx = await nativeDistributor.connect(accounts[i]).claimAll();
-          const userBalance = await ethers.provider.getBalance(accounts[i].address);
-          const txReceipt = await ethers.provider.getTransactionReceipt(tx.hash);
-          const gasUsed = txReceipt.gasUsed;
-          const gasPrice = tx.gasPrice;
-          const gasCost = gasUsed.mul(gasPrice);
-          const claimAmountWei = ethers.utils.parseEther("55.0");
+          let initialBalance = await ethers.provider.getBalance(accounts[i].address);
+          let tx = await nativeDistributor.connect(accounts[i]).claimAll();
+          let userBalance = await ethers.provider.getBalance(accounts[i].address);
+          let txReceipt = await ethers.provider.getTransactionReceipt(tx.hash);
+          let gasUsed = txReceipt.gasUsed;
+          let gasPrice = tx.gasPrice;
+          let gasCost = gasUsed.mul(gasPrice);
+          let claimAmountWei = ethers.utils.parseEther("55.0");
           expect(userBalance).to.equal(initialBalance.add(claimAmountWei).sub(gasCost)); // transfer is correct
         }
       });
