96d8ffaf94e71f67a0cb7eb5f556f8b4bdb09518
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-12-22 15:09:17 +0300

    Add deployment parameters to `deploy.js`

--- hardhat-project/scripts/deploy.js
+++ hardhat-project/scripts/deploy.js
@@ -1,12 +1,25 @@
 const hre = require("hardhat"); // Hardhat Runtime Environment
 
+const DEPLOYMENT_PARAMS = {
+  _maxDemandVolume: 3,
+  _epochCapacity: 5,
+  _epochDuration: 1000,
+  _etherMultiplier: 1,
+  _expirationBlocks: 10000,
+  _enableWithdraw: true,
+  _value: ethers.utils.parseEther("1"),
+};
+
 async function main() {
   const NativeDistributor = await hre.ethers.getContractFactory("NativeDistributor");
   const nativeDistributor = await NativeDistributor.deploy(
-    1,
-    1000,
-    true,
-    { value: ethers.utils.parseEther("1") }
+    DEPLOYMENT_PARAMS._maxDemandVolume,
+    DEPLOYMENT_PARAMS._epochCapacity,
+    DEPLOYMENT_PARAMS._epochDuration,
+    DEPLOYMENT_PARAMS._etherMultiplier,
+    DEPLOYMENT_PARAMS._expirationBlocks,
+    DEPLOYMENT_PARAMS._enableWithdraw,
+    { value: DEPLOYMENT_PARAMS._value }
   );
   await nativeDistributor.deployed();
   console.log("NativeDistributor deployed to:", nativeDistributor.address);
