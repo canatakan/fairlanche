c4acc6d8a2e37ff5198b5d7b441c333d5401ea40
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2023-01-05 17:09:01 +0300

    Fix `calculateSMFShare()`

--- hardhat-project/contracts/lib/ShareCalculator.sol
+++ hardhat-project/contracts/lib/ShareCalculator.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.8.13;
 import "./Heapified.sol";
 
 library ShareCalculator {
+    /*
+     * Share calculation functions calculate the maximum share that can be distributed
+     * in the current epoch to the users. In addition to that, they also
+     * calculate the total distribution amount for the calculated maximum
+     * share.
+     *
+     * These two values mentioned above are returned in a tuple as (share, amount).
+     *
+     * Note: They should be called after the updateState() call.
+     */
+
     function calculateQMFShare(
         uint16 maxDemandVolume,
         uint256 totalDemand,
         uint256[] calldata numberOfDemands,
         uint256 cumulativeCapacity
     ) external pure returns (uint16 _share, uint256 _amount) {
-        /*
-         * This function calculates the maximum share that can be distributed
-         * in the current epoch to the users. In addition to that,it also
-         * calculates the total distribution amount for the calculated maximum
-         * share.
-         *
-         * These two values mentioned above are returned in a tuple as (share, amount).
-         *
-         * Note: only called by updateState(), hence, assumes that the state is updated
-         */
-
         uint256 cumulativeNODSum = 0;
         uint256 cumulativeTDVSum = 0;
 
@@ -59,41 +59,38 @@ library ShareCalculator {
         uint16[] calldata epochDemands,
         uint256 cumulativeCapacity
     ) external pure returns (uint16 _share, uint256 _amount) {
-       if (epochDemands.length == 0) {
+        if (epochDemands.length == 0) {
             return (maxDemandVolume, 0);
         }
-        
+
         Heapified.Heap memory heap = Heapified.heapify(epochDemands);
 
         uint256 simulatedCapacity = cumulativeCapacity;
         uint256 heapSize = heap.heap.length;
         uint16 simulatedShare = uint16(simulatedCapacity / heapSize);
         uint16 result = 0;
-        uint256 distribution = 0;
 
-        while(heapSize > 0 && simulatedCapacity >= heapSize){
-            
-            uint256 simulatedDistribution = 0;
-            while(heap.heap[0] <= simulatedShare){
+        while (heapSize > 0 && simulatedCapacity >= heapSize) {
+            while (heap.heap[0] <= simulatedShare) {
                 simulatedCapacity -= heap.heap[0];
-                simulatedDistribution += heap.heap[0];
                 (heap, ) = Heapified.extractMin(heap);
                 heapSize--;
                 if (heapSize == 0) {
-                    distribution += simulatedDistribution;
-                    return (maxDemandVolume, distribution);
+                    return (
+                        maxDemandVolume,
+                        cumulativeCapacity - simulatedCapacity
+                    );
                 }
             }
-            
-            simulatedCapacity -= simulatedShare * heapSize;
-            distribution += simulatedShare * heapSize;
 
-            for(uint256 i = 0; i < heapSize; i++)
+            simulatedCapacity -= simulatedShare * heapSize;
+
+            for (uint256 i = 0; i < heapSize; i++)
                 heap.heap[i] -= simulatedShare;
 
             result += simulatedShare;
             simulatedShare = uint16(simulatedCapacity / heapSize);
         }
-        return (result, epochDemands.length * result);
+        return (result, cumulativeCapacity - simulatedCapacity);
     }
 }
