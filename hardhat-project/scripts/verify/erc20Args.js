2e783e8193a79278936155b086e154146b647708
cagin24 <cagin24@gmail.com>
2023-01-05 21:20:56 +0300

    delete hardhat-project and subnet

--- hardhat-project/scripts/verify/erc20Args.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-    "0x0BFA92cd54504AAeE4EEe55246e98e588C041dCE",
-    300,
-    500,
-    600,
-    1000,
-    3000,
-    true,
-];
--- hardhat-project/scripts/verify/erc20ResourceArgs.js
+++
@@ -1,8 +0,0 @@
-const { ethers } = require("hardhat");
-
-module.exports = [
-     "Test Token",
-     "TST20",
-     ethers.utils.parseEther("100000"),
-     ethers.utils.parseEther("1000000"),
-];
