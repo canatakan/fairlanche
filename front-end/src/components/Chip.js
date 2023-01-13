b60a02c0ffa52491ff5952000820a2213b142eef
cagin24 <cagin24@gmail.com>
2023-01-13 19:50:26 +0300

    change color

--- front-end/src/components/Chip.js
+++ front-end/src/components/Chip.js
@@ -6,8 +6,8 @@ export default function Chip({ value, selectedValue,handleSelectedValue }) {
       onClick={() => handleSelectedValue(value)}
       className={
         selectedValue === value
-          ? "text-white p-2 rounded-full mx-1 text-sm bg-yellow-500 border-2 border-gray-500 cursor-pointer"
-          : "text-black border-2  cursor-pointer border-gray-500 p-2 rounded-full mx-1 text-sm bg-white"
+          ? "text-white p-2 rounded-full mx-1 text-sm bg-yellow-500 border-2 border-blue-400 cursor-pointer"
+          : "text-black border-2  cursor-pointer border-blue-400 p-2 rounded-full mx-1 text-sm bg-white"
       }
     >
       {value}
