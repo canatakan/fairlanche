6a7c4b35790623ecaebd4de7b475ba0cf8eafe23
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2023-01-15 02:18:23 +0300

    Enhance blockchain container UI

--- front-end/src/components/BlockchainTxContainer.js
+++ front-end/src/components/BlockchainTxContainer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const BlockchainTxContainer = ({tx,handleAddCustomNetwork}) => {
   return (
-    <div className='w-full mx-auto border rounded-md flex justify-between items-center px-8 py-6 mb-6'>
-        <h1 className='text-lg'>Blockchain:{tx}</h1>
-        <button className="bg-red-500 text-white px-4 py-2 rounded mt-4" onClick={handleAddCustomNetwork} >Add Custom Network</button>
+    <div className='flex flex-col w-full mx-auto border-2 border-gray-300 rounded-xl px-8 py-6 mb-16'>
+        <h1 className='text-lg font-bold text-center'>{tx}</h1>
+        <button className="bg-red-500 text-white px-4 py-2 rounded mt-4" onClick={handleAddCustomNetwork} >Add Blockchain To Metamask</button>
     </div>
   )
 }
