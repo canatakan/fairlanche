c93e7da3e4cded888e9c2259e0c6793ae75f5a49
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2023-01-14 17:15:35 +0300

    Minor update in Home Page

--- front-end/src/components/pages/Home.js
+++ front-end/src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center py-10">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to Fairlanche!
