ff905c8e63fd78c5bc87d60b5bcba75b65d5033b
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2023-01-15 21:19:31 +0300

    Update `nodeIP`

--- subnet/front-end/scripts/config.js
+++ subnet/front-end/scripts/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    nodeIP: "fairlanche.com",
+    nodeIP: "fairlanche.link",
     nodePort: 9650,
     protocol: "https",
     networkID: 5, // avax: 1, fuji: 5, custom: 1337, local: 12345
